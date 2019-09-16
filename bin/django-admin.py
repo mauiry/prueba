4e800b911e7351395be47f747317eca3da5c95d6
mauri <mauriubal0@gmail.com>
2019-09-16 20:51:15 -0300

    se crea el projecto prueba

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/mauri/pruebatb1/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.7.11','django-admin.py'
+__requires__ = 'Django==1.7.11'
+__import__('pkg_resources').run_script('Django==1.7.11', 'django-admin.py')
